cd8a361798deaa4f1e8e2f9d178910286ac661d9
Wojciech Kwolek <wojciech@kwolek.io>
2022-11-20 06:04:51 +0100

    add config generator

--- go.mod
+++ go.mod
@@ -3,13 +3,16 @@ module github.com/irth/tg2fedi
 go 1.19
 
 require (
-	github.com/urfave/cli v1.22.10
+	github.com/urfave/cli/v2 v2.23.5
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/mattn/go-mastodon v0.0.6 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
-	github.com/urfave/cli/v2 v2.23.5 // indirect
+	github.com/tomnomnom/linkheader v0.0.0-20180905144013-02ca5825eb80 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 )
--- main.go
+++ main.go
@@ -1,18 +1,75 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
 
-	"github.com/urfave/cli"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/mattn/go-mastodon"
+	"github.com/urfave/cli/v2"
 	"gopkg.in/yaml.v3"
 )
 
+type MastodonConfig struct {
+	Server       string
+	ClientID     string `yaml:"client-id"`
+	ClientSecret string `yaml:"client-secret"`
+	Token        string `yaml:"token"`
+}
+
+func (m *MastodonConfig) CheckAppAuth(ctx context.Context) error {
+	if m.Server == "" {
+		return fmt.Errorf("no server provided")
+	}
+	if m.ClientID == "" {
+		return fmt.Errorf("no client ID provided")
+	}
+	if m.ClientSecret == "" {
+		return fmt.Errorf("no server ID provided")
+	}
+
+	mc := mastodon.NewClient(&mastodon.Config{
+		Server:       m.Server,
+		ClientID:     m.ClientID,
+		ClientSecret: m.ClientSecret,
+	})
+	return mc.AuthenticateApp(ctx)
+}
+
+func (m *MastodonConfig) CheckUserAuth(ctx context.Context) (*mastodon.Account, error) {
+	if m.Token == "" {
+		return nil, fmt.Errorf("no user token provided")
+	}
+	mc := mastodon.NewClient(&mastodon.Config{
+		Server:       m.Server,
+		ClientID:     m.ClientID,
+		ClientSecret: m.ClientSecret,
+	})
+	if err := mc.AuthenticateToken(ctx, m.Token, "urn:ietf:wg:oauth:2.0:oob"); err != nil {
+		return nil, err
+	}
+	return mc.GetAccountCurrentUser(ctx)
+}
+
+type TelegramConfig struct {
+	ApiToken string   `yaml:"api-token"`
+	Channels []string `yaml:"channels"`
+}
+
+func (t *TelegramConfig) CheckAuth() (*tgbotapi.User, error) {
+	bot, err := tgbotapi.NewBotAPI(t.ApiToken)
+	if err != nil {
+		return nil, err
+	}
+
+	return &bot.Self, nil
+}
+
 type Config struct {
-	Server     string `yaml:"server"`
-	ClientName string `yaml:"client-name"`
-	Website    string `yaml:"website"`
+	Mastodon MastodonConfig `yaml:"mastodon"`
+	Telegram TelegramConfig `yaml:"telegram"`
 
 	path string
 }
@@ -20,7 +77,7 @@ type Config struct {
 func LoadConfig(c *Config) error {
 	cfgPath, ok := os.LookupEnv("TG2FEDI_CFG")
 	if !ok {
-		cfgPath = "./tg2fedi.yaml"
+		cfgPath = "./tg2fedi.yml"
 	}
 
 	cfgF, err := os.Open(cfgPath)
@@ -33,6 +90,8 @@ func LoadConfig(c *Config) error {
 		return fmt.Errorf("couldn't decode config: %s: %w", cfgPath, err)
 	}
 
+	c.path = cfgPath
+
 	return nil
 }
 
@@ -43,10 +102,13 @@ func main() {
 		Action: func(*cli.Context) error {
 			var config Config
 			if err := LoadConfig(&config); err != nil {
-				log.Printf("to generate a config, run `%s gen`\n", os.Args[0])
+				log.Printf("to generate a config, run `%s setup`\n", os.Args[0])
 				return err
 			}
 			return nil
+		},
+		Commands: []*cli.Command{
+			SetupCommand(),
 		},
 	}
 
