481de65b4af28aa3e0f0aa77f0450506361406f7
Wojciech Kwolek <wojciech@kwolek.io>
2022-11-21 22:32:13 +0100

    handle media groups

--- internal/telegram/client.go
+++ internal/telegram/client.go
@@ -3,12 +3,17 @@ package telegram
 import (
 	"fmt"
 	"log"
+	"sync"
+	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 type Telegram struct {
 	Config Config
+
+	mediaGroupChannels map[string]chan *tgbotapi.Message
+	sync.Mutex
 }
 
 type Message struct {
@@ -17,6 +22,8 @@ type Message struct {
 }
 
 func (t *Telegram) StartReader() (<-chan Message, error) {
+	t.mediaGroupChannels = make(map[string]chan *tgbotapi.Message)
+
 	tg, err := tgbotapi.NewBotAPI(t.Config.ApiToken)
 	if err != nil {
 		return nil, fmt.Errorf("telegram auth failed: %w", err)
@@ -28,21 +35,78 @@ func (t *Telegram) StartReader() (<-chan Message, error) {
 
 	ch := make(chan Message)
 	go func() {
-		for msg := range tg.GetUpdatesChan(updateConfig) {
-			log.Printf("received: %+v", msg)
-			// TODO: support media
-			if msg.ChannelPost == nil {
+		for update := range tg.GetUpdatesChan(updateConfig) {
+			update := update
+
+			if update.ChannelPost == nil {
 				continue
 			}
 
-			m := Message{
-				Text: msg.ChannelPost.Text,
-			}
-
-			ch <- m
+			go func() {
+				if err := t.handleUpdate(ch, update.ChannelPost); err != nil {
+					log.Printf("telegram: handling post: %s", err)
+				}
+			}()
 		}
 		close(ch)
 	}()
 
 	return ch, nil
+}
+
+func (t *Telegram) handleUpdate(ch chan Message, msg *tgbotapi.Message) error {
+	if msg.MediaGroupID == "" {
+		// this message is not part of a group, we do not have to wait
+		return t.submitMessages(ch, []*tgbotapi.Message{msg})
+	}
+
+	t.Lock()
+	defer t.Unlock()
+
+	groupCh, ok := t.mediaGroupChannels[msg.MediaGroupID]
+	if !ok {
+		groupCh = t.handleMediaGroup(ch, msg.MediaGroupID)
+		t.mediaGroupChannels[msg.MediaGroupID] = groupCh
+	}
+	groupCh <- msg
+
+	return nil
+}
+
+func (t *Telegram) handleMediaGroup(ch chan Message, groupId string) chan *tgbotapi.Message {
+	groupCh := make(chan *tgbotapi.Message)
+
+	go func() {
+		messages := []*tgbotapi.Message{}
+		timerDuration := 10 * time.Second
+		timer := time.NewTimer(timerDuration)
+
+		for {
+			select {
+			case msg := <-groupCh:
+				messages = append(messages, msg)
+				if !timer.Stop() {
+					<-timer.C
+				}
+				timer.Reset(timerDuration)
+			case <-timer.C:
+				func() {
+					t.Lock()
+					defer t.Unlock()
+					delete(t.mediaGroupChannels, groupId)
+				}()
+				t.submitMessages(ch, messages)
+				return
+			}
+		}
+	}()
+	return groupCh
+}
+
+func (t *Telegram) submitMessages(ch chan Message, msgs []*tgbotapi.Message) error {
+	log.Printf("received msgs: %d", len(msgs))
+	for i, msg := range msgs {
+		fmt.Println(i, msg.Text, msg.Photo, msg.Caption)
+	}
+	return nil
 }
