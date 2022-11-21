c38dd664a70b405225c6d2903168a43e1f4d58d1
Wojciech Kwolek <wojciech@kwolek.io>
2022-11-21 23:52:53 +0100

    get file ids for media, also fix mediagroup ordering

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
 	github.com/mattn/go-mastodon v0.0.6
 	github.com/urfave/cli/v2 v2.23.5
+	golang.org/x/exp v0.0.0-20221114191408-850992195362
 	gopkg.in/yaml.v3 v3.0.1
 )
 
--- internal/telegram/client.go
+++ internal/telegram/client.go
@@ -7,12 +7,14 @@ import (
 	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"golang.org/x/exp/slices"
 )
 
 type Telegram struct {
 	Config Config
 
-	mediaGroupChannels map[string]chan *tgbotapi.Message
+	mediaGroupChannels map[string]chan orderedMessage
+	api                *tgbotapi.BotAPI
 	sync.Mutex
 }
 
@@ -21,29 +23,50 @@ type Message struct {
 	Media []string
 }
 
-func (t *Telegram) StartReader() (<-chan Message, error) {
-	t.mediaGroupChannels = make(map[string]chan *tgbotapi.Message)
+type orderedMessage struct {
+	no uint64
+	*tgbotapi.Message
+}
 
-	tg, err := tgbotapi.NewBotAPI(t.Config.ApiToken)
+type messageGroup []orderedMessage
+
+func (m messageGroup) Sort() {
+	slices.SortFunc(m, func(a orderedMessage, b orderedMessage) bool {
+		return a.no < b.no
+	})
+}
+
+func (t *Telegram) StartReader() (<-chan Message, error) {
+	t.mediaGroupChannels = make(map[string]chan orderedMessage)
+
+	var err error
+	t.api, err = tgbotapi.NewBotAPI(t.Config.ApiToken)
 	if err != nil {
 		return nil, fmt.Errorf("telegram auth failed: %w", err)
 	}
-	log.Printf("telegram: logged in as %s", tg.Self.UserName)
+	log.Printf("telegram: logged in as %s", t.api.Self.UserName)
 
 	updateConfig := tgbotapi.NewUpdate(0)
 	updateConfig.Timeout = 30
 
 	ch := make(chan Message)
 	go func() {
-		for update := range tg.GetUpdatesChan(updateConfig) {
+		// If we post 10 images per second, it'd take about 5.8e12 years to max
+		// out this counter. This is around 5.8e12 years after the sun explodes
+		// and swallows us all.
+		var counter uint64 = 0
+
+		for update := range t.api.GetUpdatesChan(updateConfig) {
 			update := update
+			updateId := counter
+			counter++
 
 			if update.ChannelPost == nil {
 				continue
 			}
 
 			go func() {
-				if err := t.handleUpdate(ch, update.ChannelPost); err != nil {
+				if err := t.handleUpdate(ch, orderedMessage{updateId, update.ChannelPost}); err != nil {
 					log.Printf("telegram: handling post: %s", err)
 				}
 			}()
@@ -54,10 +77,10 @@ func (t *Telegram) StartReader() (<-chan Message, error) {
 	return ch, nil
 }
 
-func (t *Telegram) handleUpdate(ch chan Message, msg *tgbotapi.Message) error {
+func (t *Telegram) handleUpdate(ch chan Message, msg orderedMessage) error {
 	if msg.MediaGroupID == "" {
 		// this message is not part of a group, we do not have to wait
-		return t.submitMessages(ch, []*tgbotapi.Message{msg})
+		return t.submitMessages(ch, []orderedMessage{msg})
 	}
 
 	t.Lock()
@@ -73,11 +96,11 @@ func (t *Telegram) handleUpdate(ch chan Message, msg *tgbotapi.Message) error {
 	return nil
 }
 
-func (t *Telegram) handleMediaGroup(ch chan Message, groupId string) chan *tgbotapi.Message {
-	groupCh := make(chan *tgbotapi.Message)
+func (t *Telegram) handleMediaGroup(ch chan Message, groupId string) chan orderedMessage {
+	groupCh := make(chan orderedMessage)
 
 	go func() {
-		messages := []*tgbotapi.Message{}
+		messages := messageGroup{}
 		timerDuration := 10 * time.Second
 		timer := time.NewTimer(timerDuration)
 
@@ -105,10 +128,50 @@ func (t *Telegram) handleMediaGroup(ch chan Message, groupId string) chan *tgbot
 	return groupCh
 }
 
-func (t *Telegram) submitMessages(ch chan Message, msgs []*tgbotapi.Message) error {
+type MediaType int
+
+const (
+	MediaTypeUnknown MediaType = 0
+	MediaTypePhoto   MediaType = iota
+	MediaTypeVideo   MediaType = iota
+)
+
+func (m MediaType) String() string {
+	return []string{"unknown", "photo", "video"}[m]
+}
+
+func (t *Telegram) submitMessages(ch chan Message, msgs messageGroup) error {
 	log.Printf("received msgs: %d", len(msgs))
-	for i, msg := range msgs {
-		fmt.Println(i, msg.Text, msg.Photo, msg.Caption)
+	msgs.Sort()
+	slices.SortFunc(msgs, func(a orderedMessage, b orderedMessage) bool {
+		return a.no < b.no
+	})
+
+	for _, msg := range msgs {
+		var mediaType MediaType
+		var fileID string
+		var uniqueID string
+		switch {
+		case len(msg.Photo) > 0:
+			largest := msg.Photo[0]
+			for _, photo := range msg.Photo {
+				if photo.Height > largest.Height {
+					largest = photo
+				}
+			}
+			fileID = largest.FileID
+			uniqueID = largest.FileUniqueID
+			mediaType = MediaTypePhoto
+		case msg.Video != nil:
+			fileID = msg.Video.FileID
+			uniqueID = msg.Video.FileUniqueID
+			mediaType = MediaTypeVideo
+		case msg.Sticker != nil:
+			fileID = msg.Sticker.FileID
+			uniqueID = msg.Sticker.FileUniqueID
+			mediaType = MediaTypePhoto
+		}
+		log.Printf("found media %s, id: %s, uid: %s", mediaType, fileID, uniqueID)
 	}
 	return nil
 }
